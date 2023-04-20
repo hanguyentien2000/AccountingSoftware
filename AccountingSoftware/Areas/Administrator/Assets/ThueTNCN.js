2307a6d1e098559a8d39ada184d343c942487b53
Ha Nguyen <hanguyentien2000@gmail.com>
2023-04-21 01:00:20 +0700

    add thuế tncn

---
+++ AccountingSoftware/Areas/Administrator/Assets/ThueTNCN.js
@@ -0,0 +1,111 @@
+﻿//load dữ liệu lên form sửa
+function loadData(id) {
+    $.ajax({
+        type: 'POST',
+        data: { "id": id },
+        url: '/ThueTNCN/Index',
+        success: function (response) {
+            $("#id1").val(response.MaThueTNCN);
+            $("#manv1").val(response.MaNV);
+            $("#bt1").val(response.BacThue);
+            $("#t1").val(response.Tu);
+            $("#d1").val(response.Den);
+        },
+        error: function (response) {
+            //debugger;  
+            console.log(xhr.responseText);
+            alert("Error has occurred..");
+        }
+    });
+}
+
+//gửi ajax thêm mới
+function themThueTNCN() {
+    let data = {};
+    let formData = $('#add-form').serializeArray({
+    });
+    $.each(formData, function (index, value) {
+        data["" + value.name + ""] = value.value;
+    });
+    $.ajax({
+        url: '/ThueTNCN/Create',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        success: function (response) {
+            if (response.status == true) {
+                $("#add-message").addClass("text-warning");
+                setTimeout(function () {
+                    window.location.replace("/Administrator/ThueTNCN/Index");
+                }, 1000)
+            } else {
+                $("#add-message").addClass("text-danger");
+            }
+            $("#add-message").html(response.message);
+        },
+        error: function (response) {
+            console.log(response);
+        }
+    });
+    return false;
+}
+
+//gửi ajax sửa danh mục
+function capNhatThueTNCN() {
+    let data = {};
+    let formData = $('#update-form').serializeArray({
+    });
+    $.each(formData, function (index, value) {
+        data["" + value.name + ""] = value.value;
+    });
+    $.ajax({
+        url: '/ThueTNCN/Update',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        success: function (response) {
+            $("#update-message").html(response.message);
+            if (response.status == true) {
+                $("#update-message").addClass("text-warning");
+                setTimeout(function () {
+                    window.location.replace("/Administrator/ThueTNCN/Index");
+                }, 1000)
+            } else {
+                $("#update-message").addClass("text-danger");
+            }
+        },
+        error: function (response) {
+            console.log(response);
+        }
+    });
+    return false;
+}
+
+//load data lên form xóa
+function deleteItem(id) {
+    $("#delete-id").val(id);
+}
+
+function xoaThueTNCN() {
+    let id = $("#delete-id").val();
+    $.ajax({
+        type: 'POST',
+        data: { "id": id },
+        url: '/ThueTNCN/Delete',
+        success: function (response) {
+            if (response.status == true) {
+                $("#row-" + id).remove();
+                $("#delete-message").addClass("text-warning");
+                setTimeout(function () {
+                    window.location.replace("/Administrator/ThueTNCN/Index");
+                }, 1000)
+            }
+        },
+        error: function (response) {
+            alert("Error has occurred..");
+            console.log(response);
+        }
+    });
+}
---
+++ AccountingSoftware/Areas/Administrator/Controllers/ThueTNCNController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AccountingSoftware.Models;
+using PagedList;
+
+namespace AccountingSoftware.Areas.Administrator.Controllers
+{
+    public class ThueTNCNController : Controller
+    {
+        private AccountingSoftwareDbContext db = new AccountingSoftwareDbContext();
+
+        // GET: Administrator/ThueTNCN
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
+        {
+            var thueTNCNs = db.ThueTNCNs.Include(t => t.NhanVien);
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                thueTNCNs = thueTNCNs.Where(dm => dm.NhanVien.HoTen.Contains(searchString));
+            }
+            return View(thueTNCNs.OrderBy(dm => dm.MaThueTNCN).ToPagedList(page, pageSize));
+        }
+
+        // GET: Administrator/ThueTNCN/Details/5
+        [HttpPost]
+        public JsonResult Index(int id)
+        {
+            ThueTNCN tk = db.ThueTNCNs.Where(a => a.MaThueTNCN.Equals(id)).FirstOrDefault();
+            return Json(tk, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Administrator/ThueTNCN/Create
+        [HttpPost]
+        public JsonResult Create(ThueTNCN tk)
+        {
+            try
+            {
+                var existData = db.ThueTNCNs.Where(x => x.NhanVien.MaNV == tk.NhanVien.MaNV).FirstOrDefault();
+                if (existData == null)
+                {
+                    db.ThueTNCNs.Add(tk);
+                    db.SaveChanges();
+                    return Json(new { status = true, message = "Thêm thành công" });
+                }
+                else
+                    return Json(new { status = false, message = "Người dùng này đã tồn tại tài khoản" });
+            }
+            catch (Exception)
+            {
+                return Json(new { status = false, message = "Đã có lỗi xảy ra" });
+            }
+        }
+
+        // GET: Administrator/ThueTNCN/Edit/5
+        [HttpPost]
+        public JsonResult Update(ThueTNCN tk)
+        {
+            try
+            {
+                ThueTNCN update = db.ThueTNCNs.Where(a => a.MaThueTNCN.Equals(tk.MaThueTNCN)).FirstOrDefault();
+                update.Tu = tk.Tu;
+                update.Den = tk.Den;
+                update.BacThue = tk.BacThue;
+                update.MaNV = tk.MaNV;
+                db.Entry(update).State = EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { status = true, message = "Sửa thông tin thành công" });
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return Json(new { status = false, message = "Đã có lỗi xảy ra" });
+            }
+        }
+
+        // GET: Administrator/ThueTNCN/Delete/5
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                ThueTNCN tk = db.ThueTNCNs.Where(a => a.MaThueTNCN.Equals(id)).FirstOrDefault();
+                db.ThueTNCNs.Remove(tk);
+                db.SaveChanges();
+                return Json(new { status = true, message = "Xoá thành công" });
+            }
+            catch (Exception)
+            {
+                return Json(new { status = false });
+            }
+        }
+
+        // POST: Administrator/ThueTNCN/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ThueTNCN thueTNCN = db.ThueTNCNs.Find(id);
+            db.ThueTNCNs.Remove(thueTNCN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
