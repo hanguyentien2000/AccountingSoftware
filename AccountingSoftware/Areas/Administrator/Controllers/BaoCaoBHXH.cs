e47075f2d2289038e8fd629cb823651e11d4147e
Ha Nguyen <hanguyentien2000@gmail.com>
2023-05-05 01:01:50 +0700

    tham số tính lương chưa done

---
+++ AccountingSoftware/Areas/Administrator/Controllers/BaoCaoBHXH.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AccountingSoftware.Models;
+using PagedList;
+
+namespace AccountingSoftware.Areas.Administrator.Controllers
+{
+    public class BaoCaoBHXHController : Controller
+    {
+        private AccountingSoftwareDbContext db = new AccountingSoftwareDbContext();
+
+        // GET: Administrator/BaoCaoBHXH
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
+        {
+            var BaoCaoBHXHs = db.NhanViens.Select(kh => kh);
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                BaoCaoBHXHs = BaoCaoBHXHs.Where(dm => dm.HoTen.Contains(searchString));
+            }
+            return View(BaoCaoBHXHs.OrderBy(dm => dm.MaNV).ToPagedList(page, pageSize));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AccountingSoftware/Areas/Administrator/Controllers/ThamSoTinhLuongController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AccountingSoftware.Models;
+
+namespace AccountingSoftware.Areas.Administrator.Controllers
+{
+    public class ThamSoTinhLuongController : Controller
+    {
+        private AccountingSoftwareDbContext db = new AccountingSoftwareDbContext();
+
+        // GET: Administrator/ThamSoTinhLuong
+        public ActionResult Index()
+        {
+            return View(db.ThamSoTinhLuongs.ToList());
+        }
+
+        // GET: Administrator/ThamSoTinhLuong/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThamSoTinhLuong thamSoTinhLuong = db.ThamSoTinhLuongs.Find(id);
+            if (thamSoTinhLuong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thamSoTinhLuong);
+        }
+
+        // GET: Administrator/ThamSoTinhLuong/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administrator/ThamSoTinhLuong/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaThamSo,ThangNam,CongLuong,GioCongNgay,HsNgayThuong,HsNgayLe,PcAn,TLBHXH,TLBHYT,TLBHTN,TLKPCD,GiamTruBanThan,GiamTruNPT")] ThamSoTinhLuong thamSoTinhLuong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ThamSoTinhLuongs.Add(thamSoTinhLuong);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(thamSoTinhLuong);
+        }
+
+        // GET: Administrator/ThamSoTinhLuong/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThamSoTinhLuong thamSoTinhLuong = db.ThamSoTinhLuongs.Find(id);
+            if (thamSoTinhLuong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thamSoTinhLuong);
+        }
+
+        // POST: Administrator/ThamSoTinhLuong/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaThamSo,ThangNam,CongLuong,GioCongNgay,HsNgayThuong,HsNgayLe,PcAn,TLBHXH,TLBHYT,TLBHTN,TLKPCD,GiamTruBanThan,GiamTruNPT")] ThamSoTinhLuong thamSoTinhLuong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thamSoTinhLuong).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(thamSoTinhLuong);
+        }
+
+        // GET: Administrator/ThamSoTinhLuong/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThamSoTinhLuong thamSoTinhLuong = db.ThamSoTinhLuongs.Find(id);
+            if (thamSoTinhLuong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thamSoTinhLuong);
+        }
+
+        // POST: Administrator/ThamSoTinhLuong/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ThamSoTinhLuong thamSoTinhLuong = db.ThamSoTinhLuongs.Find(id);
+            db.ThamSoTinhLuongs.Remove(thamSoTinhLuong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
