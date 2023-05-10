a60363326ada05e217341c7611d62d23f63a7089
hanguyentien2000@gmail.com <hanguyentien2000@gmail.com>
2023-05-10 15:17:24 +0700

    Update done báo cáo bảo hiểm xã hội

--- AccountingSoftware/Areas/Administrator/Controllers/BaoCaoBaoHiemXHController.cs
+++ AccountingSoftware/Areas/Administrator/Controllers/BaoCaoBaoHiemXHController.cs
@@ -17,31 +17,40 @@ namespace AccountingSoftware.Areas.Administrator.Controllers
         // GET: Administrator/BaoCaoBaoHiemXH
         public ActionResult Index(string thang, string nam)
         {
+            var listModel = new List<BaoCaoBaoHiemXH>();
+
             var parameters = thang + "/" + nam;
-            var BaoCaoBHXHs = db.NhanViens.Select(kh => kh);
             var paramsThamSo = db.ThamSoTinhLuongs.Where(x => x.ThangNam == parameters).FirstOrDefault();
-            var listBHXH = new List<BaoCaoBaoHiemXH>();
-            var listNhanVien = db.NhanViens.Select(x => x);
-            foreach (var item in listNhanVien)
+            if (paramsThamSo == null)
             {
-                foreach (var item1 in listBHXH)
-                {
-                    item1.HoTen = item.HoTen;
-                    item1.LuongCoBan = item.LuongCoBan;
-                    item1.TLBHXH = paramsThamSo.TLBHXH;
-                    item1.TLBHTN = paramsThamSo.TLBHTN;
-                    item1.TLBHYT = paramsThamSo.TLBHYT;
-                    int total1 = int.Parse(item.LuongCoBan) * int.Parse(paramsThamSo.TLBHXH);
-                    item1.MucChiTraTLBHTN = total1.ToString();
-                    int total2 = int.Parse(item.LuongCoBan) * int.Parse(paramsThamSo.TLBHTN);
-                    item1.MucChiTraTLBHTN = total2.ToString();
-                    int total3 = int.Parse(item.LuongCoBan) * int.Parse(paramsThamSo.TLBHYT);
-                    item1.MucChiTraTLBHYT = total3.ToString();
-                    int total4 = total1 + total2 + total3;
-                    item1.TongTien = total4.ToString();
-                }
+                var newModel = new BaoCaoBaoHiemXH();
+
+                return View(newModel);
             }
-            return View(listBHXH);
+            else
+            {
+                var listNhanVien = db.NhanViens.ToList();
+                foreach (var item in listNhanVien)
+                {
+                    var newModel = new BaoCaoBaoHiemXH();
+                    newModel.HoTen = item.HoTen;
+                    newModel.LuongCoBan = item.LuongCoBan;
+                    newModel.TLBHXH = paramsThamSo.TLBHXH;
+                    newModel.TLBHTN = paramsThamSo.TLBHTN;
+                    newModel.TLBHYT = paramsThamSo.TLBHYT;
+                    int total1 = int.Parse(item.LuongCoBan) * int.Parse(paramsThamSo.TLBHXH);
+                    newModel.MucChiTraTLBHXH = total1.ToString();
+                    int total2 = int.Parse(item.LuongCoBan) * int.Parse(paramsThamSo.TLBHTN);
+                    newModel.MucChiTraTLBHTN = total2.ToString();
+                    int total3 = int.Parse(item.LuongCoBan) * int.Parse(paramsThamSo.TLBHYT);
+                    newModel.MucChiTraTLBHYT = total3.ToString();
+                    int total4 = total1 + total2 + total3;
+                    newModel.TongTien = total4.ToString();
+                    listModel.Add(newModel);
+                }
+                ViewBag.ListBhxh = listModel;
+                return View(ViewBag.ListBhxh);
+            }
         }
 
         protected override void Dispose(bool disposing)
